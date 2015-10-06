dc114fe863448a5e118fa4694fd0408a12062da2
tongrhj <tongrhj@gmail.com>
2015-10-06 11:35:05 +0000

    fixed moneyDisplay

--- js/data.js
+++ js/data.js
@@ -1,16 +1,15 @@
 var player = {
-    money: 0.0,
     moneyPerClick: 1,
     totalMoneyEarned: 0
 }
 
 function addMoney(money) {
-	player.totalMoneyEarned = player.money + money;
+	player.totalMoneyEarned += money;
 }
 
 $(document).ready(function(){
     $("#addmoneyBtn").click(function(){
         addMoney(player.moneyPerClick);
-        $('#moneyDisplay').innerHTML="player.totalMoneyEarned";
+        document.getElementById('moneyDisplay').innerHTML = player.totalMoneyEarned;
     });    
 });
