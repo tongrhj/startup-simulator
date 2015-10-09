64ffb00c14002c7ea6c0602ac9541400d661f866
tongrhj <tongrhj@gmail.com>
2015-10-10 01:28:10 +0800

    Added addmoneyBtn, player money, moneyPerSecond, moneyPerClick

--- js/data.js
+++ js/data.js
@@ -1,41 +1,5 @@
-var Game = function() {
-
-  this.monies = 0;
-  this.moneyRatePerSecond = 1000;
-  // Cache a bound onFrame since we need it each frame.
-    this.onFrame = this.onFrame.bind(this);
-
-  // Restart the onFrame loop
-    this.lastFrame = +new Date() / 1000;
-    requestAnimFrame(this.onFrame);
+var player = {
+  money: 0,
+  moneyPerSecond: 0,
+  moneyPerClick: 1
 };
-Game.prototype.onFrame = function() {
-
-    var now = +new Date() / 1000,
-        delta = now - this.lastFrame;
-    this.lastFrame = now;
-
-  this.monies += this.moneyRatePerSecond*delta;
-  //document.title = ""+Math.round(this.monies);
-  document.getElementById('moneyDisplay').innerHTML = Math.round(this.monies);
-  console.log(Math.round(this.monies));
-    // Request next frame.
-    requestAnimFrame(this.onFrame);
-};
-
-
-/**
-   * Cross browser RequestAnimationFrame
-   */
-  var requestAnimFrame = (function() {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function(/* function */ callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-  })();
-
-var gameInstance = new Game();
---
+++ js/engine.js
@@ -0,0 +1,55 @@
+var Game = function() {
+
+  // Cache a bound onFrame since we need it each frame.
+    this.onFrame = this.onFrame.bind(this);
+
+  // Restart the onFrame loop
+    this.lastFrame = +new Date() / 1000;
+    requestAnimFrame(this.onFrame);
+};
+Game.prototype.onFrame = function() {
+
+    var now = +new Date() / 1000,
+        delta = now - this.lastFrame;
+    this.lastFrame = now;
+
+  // Add Money per second per delta
+  player.money += player.moneyPerSecond*delta;
+  //document.title = ""+Math.round(this.monies);
+  $('moneyDisplay').innerHTML = Math.round(this.monies);
+  console.log(Math.round(this.monies));
+    // Request next frame.
+    requestAnimFrame(this.onFrame);
+};
+
+
+/**
+   * Cross browser RequestAnimationFrame
+   */
+  var requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(/* function */ callback) {
+          window.setTimeout(callback, 1000 / 60);
+        };
+  })();
+
+var gameInstance = new Game();
+
+function addMoney(money) {
+  player.money += money;
+  if (player.money < 0) player.money = 0;
+  var moneytoDisplay = Math.round((player.money*100)/100);
+  $('moneyDisplay').innerHTML = Math.round(moneytoDisplay);
+}
+
+//Initalise on game start: addmoneyBtn, etc.
+$(document).ready(function(){
+  $("#addmoneyBtn").click(function(){
+    addMoney(player.moneyPerClick);
+  })
+
+});
